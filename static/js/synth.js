1caa4a40c8c1a3419934bac1a1a20372aff9f67c
Aiden Castillo <40736622@live.napier.ac.uk>
2025-04-19 14:29:05 -0600

    Added a function to calculate the frequency of a note

--- static/js/synth.js
+++ static/js/synth.js
@@ -1,7 +1,7 @@
 const keys = document.querySelectorAll(".white-key, .black-key")
 const audioCtx = new (AudioContext || webkitAudioContext)();
 let midi = null;
-let wave = null;
+let wave = undefined;
 
 audioCtx.resume();
 const mainGainNode = audioCtx.createGain();
@@ -9,33 +9,33 @@ mainGainNode.connect(audioCtx.destination);
 mainGainNode.gain.value = 1;
 
 const synthNotesMap = {
-    c3: { frequency: 130.8128, key: "KeyQ", midi: 48 },
-    cs3: { frequency: 138.5913, key: "KeyW", midi: 49 },
-    d3: { frequency: 146.8324, key: "KeyE", midi: 50 },
-    ds3: { frequency: 155.5635, key: "KeyR", midi: 51 },
-    e3: { frequency: 164.8138, key: "KeyT", midi: 52 },
-    f3: { frequency: 174.6141, key: "KeyY", midi: 53 },
-    fs3: { frequency: 184.9972, key: "KeyU", midi: 54 },
-    g3: { frequency: 195.9977, key: "KeyI", midi: 55 },
-    gs3: { frequency: 207.6523, key: "KeyO", midi: 56 },
-    a3: { frequency: 220.0000, key: "KeyP", midi: 57 },
-    as3: { frequency: 233.0819, key: "BracketLeft", midi: 58 },
-    b3: { frequency: 246.9417, key: "BracketRight", midi: 59 },
+    c3: { frequency: 130.8128, key: "KeyQ" },
+    cs3: { frequency: 138.5913, key: "KeyW" },
+    d3: { frequency: 146.8324, key: "KeyE" },
+    ds3: { frequency: 155.5635, key: "KeyR" },
+    e3: { frequency: 164.8138, key: "KeyT" },
+    f3: { frequency: 174.6141, key: "KeyY" },
+    fs3: { frequency: 184.9972, key: "KeyU" },
+    g3: { frequency: 195.9977, key: "KeyI" },
+    gs3: { frequency: 207.6523, key: "KeyO" },
+    a3: { frequency: 220.0000, key: "KeyP" },
+    as3: { frequency: 233.0819, key: "BracketLeft" },
+    b3: { frequency: 246.9417, key: "BracketRight" },
 
-    c4: { frequency: 261.6256, key: "KeyA", midi: 60 },
-    cs4: { frequency: 277.1826, key: "KeyS", midi: 61 },
-    d4: { frequency: 293.6648, key: "KeyD", midi: 62 },
-    ds4: { frequency: 311.1270, key: "KeyF", midi: 63 },
-    e4: { frequency: 329.6276, key: "KeyG", midi: 64 },
-    f4: { frequency: 349.2282, key: "KeyH", midi: 65 },
-    fs4: { frequency: 369.9944, key: "KeyJ", midi: 66 },
-    g4: { frequency: 391.9954, key: "KeyK", midi: 67 },
-    gs4: { frequency: 415.3047, key: "KeyL", midi: 68 },
-    a4: { frequency: 440.0000, key: "Semicolon", midi: 69 },
-    as4: { frequency: 466.1638, key: "Quote", midi: 70 },
-    b4: { frequency: 493.8833, key: "KeyZ", midi: 71 },
+    c4: { frequency: 261.6256, key: "KeyA" },
+    cs4: { frequency: 277.1826, key: "KeyS" },
+    d4: { frequency: 293.6648, key: "KeyD" },
+    ds4: { frequency: 311.1270, key: "KeyF" },
+    e4: { frequency: 329.6276, key: "KeyG" },
+    f4: { frequency: 349.2282, key: "KeyH" },
+    fs4: { frequency: 369.9944, key: "KeyJ" },
+    g4: { frequency: 391.9954, key: "KeyK" },
+    gs4: { frequency: 415.3047, key: "KeyL" },
+    a4: { frequency: 440.0000, key: "Semicolon" },
+    as4: { frequency: 466.1638, key: "Quote" },
+    b4: { frequency: 493.8833, key: "KeyZ" },
 
-    c5: { frequency: 523.2511, key: "KeyX", midi: 72 }
+    c5: { frequency: 523.2511, key: "KeyX" }
 };
 
 const frequencies = Object.values(synthNotesMap).map(note => note.frequency);
@@ -48,7 +48,7 @@ const frequencies = Object.values(synthNotesMap).map(note => note.frequency);
 
 // Remember: MediaStream Recording API & Midi API
 
-function playNote(noteFrequency, wave = "sine") {
+function playNote(noteFrequency, wave = "square") {
     const osc = new OscillatorNode(audioCtx, {
         type: wave,
         frequency: noteFrequency,
@@ -67,13 +67,19 @@ function stopNote(osc, noteFrequency) {
     }
 }
 
-function calculateFrequency(noteNumber) {
+function calculateFrequency(noteNumber, isMidi = false) {
+    if (isMidi) {
+        return 2 ** ((noteNumber - 69) / 12) * 440;
+    }
+
     return 2 ** ((noteNumber - 49) / 12) * 440;
 }
 
 // Handles on-screen piano keyboard
 keys.forEach((key, index) => {
-    key.dataset.frequency = frequencies[index];
+    key.dataset.noteNumber = index + 28;
+    
+    key.dataset.frequency = calculateFrequency(key.dataset.noteNumber);
     let osc = null;
 
     // Todo: - Implement with mouseover too
@@ -117,8 +123,10 @@ if (navigator.requestMIDIAccess) {
             input.onmidimessage = (message) => {
                 const [status, note, velocity] = message.data;
 
+                const frequency = calculateFrequency(note, true);
+
                 if (velocity > 0) {
-                    osc = playNote(440);
+                    osc = playNote(frequency);
                 } else if (velocity === 0) {
                     stopNote(osc);
                 }
